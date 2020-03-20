0cab51fb74f4acc83858eed2ab642d982bb5191d
YA <YA-androidapp@users.noreply.github.com>
2020-03-21 00:22:26 +0900

    Add audio.js

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" type="text/css" href="/css/audio.css">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -20,23 +21,95 @@
                         System Administrator
                         @endcan
 
-                        @can('admin-higher') {{-- 管理者権限以上に表示される --}}
-                        <form method="POST" action="/music" enctype="multipart/form-data" >
-                            {{ csrf_field() }}
-                            <input type="file" name="audios[]" multiple>
-                            <input type="submit">
-                        </form>
-
-                        <ul>
-                            @foreach ($musics as $music)
-                                <li><a href="{{ $music->path }}" target="blank_">{{$music->artist}} / {{$music->title}}</a></li>
-                            @endforeach
-                        </ul>
-                        @endcan
                     </span>
                 </div>
             </div>
         </div>
+
+        @can('admin-higher') {{-- 管理者権限以上に表示される --}}
+            <div class="col-md-8">
+                <div class="jumbotron mt-4">
+                    <h1 class="display-4">Player</h1>
+                    <hr class="my-4">
+                    <audio autoplay preload="auto"></audio>
+                </div>
+
+                <ol id="playlist" class="list-group mt-4 col-md-12">
+                    @foreach ($musics as $music)
+                        <li class="list-group-item"><a href="#" data-src="{{ $music->path }}" target="blank_">{{$music->artist}} / {{$music->title}}</a></li>
+                    @endforeach
+                </ol>
+            </div>
+        @endcan
+
+    </div>
+    <div class="col-md-8" id="shortcuts">
+        <div class="row">
+            @can('admin-higher') {{-- 管理者権限以上に表示される --}}
+                <div class="col">
+                    <form method="POST" action="/music" enctype="multipart/form-data" >
+                        {{ csrf_field() }}
+                        <input type="file" name="audios[]" multiple>
+                        <input type="submit">
+                    </form>
+                </div>
+            @endcan
+            <div class="col">
+                <h1>Keyboard shortcuts:</h1>
+                <p><em>&rarr;</em> Next track</p>
+                <p><em>&larr;</em> Previous track</p>
+                <p><em>Space</em> Play/pause</p>
+            </div>
+        </div>
     </div>
 </div>
+
+<script src="/js/jquery.js"></script>
+<script src="/js/audio.js"></script>
+<script>
+    $(function() {
+        // Setup the player to autoplay the next track
+        var a = audiojs.createAll({
+            trackEnded: function() {
+            var next = $('ol li.playing').next();
+            if (!next.length) next = $('ol li').first();
+            next.addClass('playing').siblings().removeClass('playing');
+            audio.load($('a', next).attr('data-src'));
+            audio.play();
+            }
+        });
+
+        // Load in the first track
+        var audio = a[0];
+            first = $('ol a').attr('data-src');
+        $('ol li').first().addClass('playing');
+        audio.load(first);
+
+        // Load in a track on click
+        $('ol li').click(function(e) {
+            e.preventDefault();
+            $(this).addClass('playing').siblings().removeClass('playing');
+            audio.load($('a', this).attr('data-src'));
+            audio.play();
+        });
+        // Keyboard shortcuts
+        $(document).keydown(function(e) {
+            var unicode = e.charCode ? e.charCode : e.keyCode;
+                // right arrow
+            if (unicode == 39) {
+            var next = $('li.playing').next();
+            if (!next.length) next = $('ol li').first();
+            next.click();
+            // back arrow
+            } else if (unicode == 37) {
+            var prev = $('li.playing').prev();
+            if (!prev.length) prev = $('ol li').last();
+            prev.click();
+            // spacebar
+            } else if (unicode == 32) {
+            audio.playPause();
+            }
+        })
+    });
+</script>
 @endsection
