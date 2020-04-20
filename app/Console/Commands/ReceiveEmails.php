343828eb6fb89f98618389cddf307864735f6c3a
YA <YA-androidapp@users.noreply.github.com>
2020-04-21 08:48:22 +0900

    Retrieve files from mails

--- app/Console/Commands/ReceiveEmails.php
+++ app/Console/Commands/ReceiveEmails.php
@@ -75,7 +75,7 @@ class ReceiveEmails extends Command
                 $header = imap_header($mbox, $msgno);
                 $subject = $this->getSubject($header);
                 dump($msgno . ':' . $subject);
-                dump($this->getBody($mbox, $msgno));
+                $this->getBody($mbox, $msgno);
             }
             imap_close($mbox);
             dump('imap_close');
@@ -99,10 +99,122 @@ class ReceiveEmails extends Command
         return $subject;
     }
 
+    // protected $extensionList = array('mp3', 'pdf', 'png');
+    protected $saveDir = './';
+
     private function getBody($mbox, $msgno)
     {
-        $body = imap_body($mbox, $msgno);
-        $body = mb_convert_encoding($body, 'UTF-8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS');
-        return $body;
+        dump('getBody($mbox, $msgno): ' . print_r($mbox, true), print_r($msgno, true));
+
+        $charset = null;
+        $encoding = null;
+        $attached_data = null;
+        $parameters = null;
+
+        $info = imap_fetchstructure($mbox, $msgno);
+        dump('$info');
+        if (!empty($info->parts)) {
+            dump('!empty($info->parts)');
+            $parts_cnt = count($info->parts);
+            for ($p = 0; $p < $parts_cnt; $p++) {
+                dump('$info->parts[' . $p . ']: ' . print_r($info->parts[$p], true));
+
+                if ($info->parts[$p]->type == 0) {
+                    if (empty($charset)) {
+                        $charset = $info->parts[$p]->parameters[0]->value;
+                    }
+                    if (empty($encoding)) {
+                        $encoding = $info->parts[$p]->encoding;
+                    }
+                } elseif (!empty($info->parts[$p]->parts) && $info->parts[$p]->parts[$p]->type == 0) {
+                    $parameters = $info->parts[$p]->parameters[0]->value;
+                    if (empty($charset)) {
+                        $charset = $info->parts[$p]->parts[$p]->parameters[0]->value;
+                    }
+                    if (empty($encoding)) {
+                        $encoding = $info->parts[$p]->parts[$p]->encoding;
+                    }
+                } elseif ($info->parts[$p]->type == 3 || $info->parts[$p]->type == 4 || $info->parts[$p]->type == 5) {
+                    $files = imap_mime_header_decode($info->parts[$p]->parameters[0]->value);
+                    if (!empty($files) && is_array($files)) {
+                        $attached_data[$p]['file_name'] = null;
+                        foreach ($files as $key => $file) {
+                            if ($file->charset != 'default') {
+                                $attached_data[$p]['file_name'] .= mb_convert_encoding($file->text, 'UTF-8', $file->charset);
+                            } else {
+                                $attached_data[$p]['file_name'] .= $file->text;
+                            }
+                        }
+                    }
+                    $attached_data[$p]['content_type'] = $info->parts[$p]->subtype;
+                }
+            }
+        } else {
+            dump('empty($info->parts)');
+            $charset = $info->parameters[0]->value;
+            $encoding = $info->encoding;
+        }
+        if (empty($charset)) {
+            dump('empty($charset)');
+        }
+
+        $body = imap_fetchbody($mbox, $msgno, 1, FT_INTERNAL);
+        $body = trim($body);
+        dump('trim($body)');
+
+        if (!empty($body)) {
+            dump('!empty($body)');
+
+            switch ($encoding) {
+                case 0:
+                    $mail[$msgno]['body'] = mb_convert_encoding($body, "UTF-8", $charset);
+                    break;
+                case 1:
+                    $encode_body = imap_8bit($body);
+                    $encode_body = imap_qprint($encode_body);
+                    $mail[$msgno]['body'] = mb_convert_encoding($encode_body, "UTF-8", $charset);
+                    break;
+                case 3:
+                    $encode_body = imap_base64($body);
+                    $mail[$msgno]['body'] = mb_convert_encoding($encode_body, "UTF-8", $charset);
+                    break;
+                case 4:
+                    $encode_body = imap_qprint($body);
+                    $mail[$msgno]['body'] = mb_convert_encoding($encode_body, 'UTF-8', $charset);
+                    break;
+                case 2:
+                case 5:
+                default:
+                    dump('$encoding: ' . $encoding);
+                    break;
+            }
+        } else {
+            dump('empty($body)');
+        }
+
+        if (!empty($attached_data)) {
+            dump('!empty($attached_data)');
+            foreach ($attached_data as $key => $value) {
+                $attached = imap_fetchbody($mbox, $msgno, $key + 1, FT_INTERNAL);
+                if (empty($attached)) break;
+
+                list($name, $ex) = explode('.', $value['file_name']);
+                $mail[$msgno]['attached_file'][$key]['file_name'] = $name . '_' . time() . '_' . $key . '.' . $ex;
+                $mail[$msgno]['attached_file'][$key]['content_body'] = imap_base64($attached);
+                $mail[$msgno]['attached_file'][$key]['content_type'] = strtolower($value['content_type']);
+
+                dump('$mail: ' . print_r($mail[$msgno]['attached_file'][$key]['content_type'], true));
+
+                $body = $mail[$msgno]['attached_file'][$key]['content_body'];
+                $saveFilename = $mail[$msgno]['attached_file'][$key]['file_name'];
+                $savePath = $this->saveDir . $saveFilename;
+                if ($fp = fopen($savePath, "w")) {
+                    $length = strlen($body);
+                    fwrite($fp, $body, $length);
+                    fclose($fp);
+                    dump('Saved: ' . $saveFilename);
+                }
+            }
+        }
     }
 }
