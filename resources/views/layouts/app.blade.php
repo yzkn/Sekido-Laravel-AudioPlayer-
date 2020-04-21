df383f5688a33cbd4e323bd4b2d96966eb6271fc
YA <YA-androidapp@users.noreply.github.com>
2020-04-21 19:33:37 +0900

    Change the appname

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand px-5" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel-AudioPlayer') }}
+                {{ config('app.name', 'Sekido') }}
             </a>
             <button class="navbar-toggler px-5" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
